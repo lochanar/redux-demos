6b0c23c8b5f654e44d2e68449a17c6bc38e1c2a5
lran1347 <lochana.ranaweera@syscolabs.com>
2020-07-19 20:10:04 +0530

    Created new Courses component

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,67 +5,14 @@ import { PropTypes } from "prop-types";
 import { bindActionCreators } from "redux";
 
 class CoursesPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      course: {
-        title: ""
-      }
-    };
-
-    // OPTION 2: Binding in the constructor
-    // This is better than OPTION 1 because the function is bound once, no reallocations on each render
-    // this.handleOnChange = this.handleOnChange.bind(this);
-  }
-
-  // You could also get rid of constructor to instantiate state
-  // Can declare state as a class field in OPTION 3, but no access to props this way
-  // state = {
-  //   course: {
-  //     title: ""
-  //   }
-
-  // Functions usually inherit `this` context of the caller
-  // In this case, `this` refers to context of the caller - change handler `handleOnChange`
-  // But we need `this` to refer to our instance of the component
-  // handleOnChange(event) {
-  //   const course = { ...this.state.course, title: event.target.value };
-  //   this.setState({ course: course });
-  // }
-
-  // OPTION 3:This is called a class field
-  // Arrow functions inherit `this` from their enclosing scope, so it will reference our class instance
-  // This is because arrow funcs don't have a binding inside
-  handleOnChange = event => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course: course });
-  };
-
-  handleOnSubmit = event => {
-    event.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-  };
-
   render() {
     return (
-      // Adding submit handler to form rather than to submit button to make sure submit occurs on `Enter`
-      <form onSubmit={this.handleOnSubmit}>
+      <>
         <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input
-          type="text"
-          // OPTION 1: to bind `this` of callers to class instance, but this is not ideal
-          // Calling bind() within render causes a new function to be created on each render
-          // onChange={this.handleOnChange.bind(this)}
-          onChange={this.handleOnChange}
-          value={this.state.course.title}
-        />
-        <input type="submit" value="Save" />
         {this.props.courses.map(course => (
           <div key={course.title}>{course.title}</div>
         ))}
-      </form>
+      </>
     );
   }
 }
@@ -75,9 +22,6 @@ CoursesPage.propTypes = {
   actions: PropTypes.object.isRequired
 };
 
-// Determines what part of the Redux state we pass in as props
-// Be SPECIFIC by requesting only the data the component needs from store
-// otherwise the component can rerender for unrelated changes in state
 function mapStateToProps(state) {
   return {
     courses: state.courses
